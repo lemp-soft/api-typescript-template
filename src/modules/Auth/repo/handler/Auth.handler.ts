31368081a1aa13cfde68c24490e798f7a7e4bebb
felipe132005 <soporte@lempsoft.com>
2024-09-20 11:10:27 -0500

    no devolver contrase;a

--- src/modules/Auth/repo/handler/Auth.handler.ts
+++ src/modules/Auth/repo/handler/Auth.handler.ts
@@ -71,7 +71,11 @@ export class AuthHandler {
           return res.status(403).json({ error: 'Invalid token' })
         }
         const userResponse = await this.userRepository.findById(user.id)
-        return res.status(200).json(userResponse)
+        if (userResponse == null) {
+          return res.status(404).json({ error: 'User not found' })
+        }
+        const { password, ...userResponseWithoutPassword } = userResponse
+        return res.status(200).json(userResponseWithoutPassword)
       }) as unknown as Response
     } catch (error) {
       return res.status(400).json({ error })
